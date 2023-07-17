5866e317a1216a134588c4594b44a52d7a81b41e
Josef Katič <josef@joka00.dev>
2023-07-17 03:37:25 +0200

    Remove nur from outputs

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, nur, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       inherit (self) outputs;
       lib = nixpkgs.lib // home-manager.lib;
--- hosts/T14/networking.nix
+++ hosts/T14/networking.nix
@@ -1,8 +1,15 @@
 {
+  systemd.network.enable = true;
   networking = {
     hostName = "T14";
+    useNetworkd = true;
+    firewall = {
+      enable = true;
+      checkReversePath = "loose";
+    };
     networkmanager = {
       enable = true;
+      dns = "systemd-resolved";
     };
   };
 }
