935f641b0f750cbadb1dc8e3e4651b36ee9212c5
Josef Katič <josef@joka00.dev>
2023-07-17 10:43:15 +0200

    Changed display to monitor in hyprland rules

--- home/joka/features/desktop/hyprland/monitors.nix
+++ home/joka/features/desktop/hyprland/monitors.nix
@@ -7,7 +7,7 @@ lib.concatStringsSep "\n" (lib.forEach enabledMonitors (m:
     monitor=${m.name},${toString m.width}x${toString m.height}@${toString m.refreshRate},${toString m.x}x${toString m.y},1
     ${lib.concatStringsSep "\n" (lib.forEach m.workspaces (w: 
     ''
-    workspace=${toString w.id},display:${w.monitor},default:${lib.boolToString w.default}
+    workspace=${toString w.id},monitor:${w.monitor},default:${lib.boolToString w.default}
     ''
     )
   )}
