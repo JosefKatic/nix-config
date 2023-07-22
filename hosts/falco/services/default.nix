54d0b9821c7c011d504899007d02c81d9e9e12b5
Josef Katič <josef@joka00.dev>
2023-07-22 14:40:04 +0200

    Remove some not ready services

--- hosts/falco/services/default.nix
+++ hosts/falco/services/default.nix
@@ -4,13 +4,12 @@
     ../../common/optional/mysql.nix
     ../../common/optional/postgres.nix
 
-    ./firefly.nix
+    # ./firefly.nix
     ./files-server.nix
     ./git-remote.nix
     ./headscale.nix
     ./prometheus.nix
 
     ./cgit
-    ./website
   ];
 }
