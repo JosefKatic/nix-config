df2c697d07d2172a01610d6baa79b666d93b50ed
Josef Katič <josef@joka00.dev>
2023-12-03 03:31:42 +0100

    hosts/strix: Merge configs to default.nix

--- hosts/strix/default.nix
+++ hosts/strix/default.nix
@@ -1,22 +1,27 @@
-{
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
     ./services
 
     ../common/global
     ../common/users/joka
-
-    ./kernel.nix
   ];
 
   networking = {
     hostName = "strix";
     useDHCP = true;
   };
-  system.stateVersion = "23.05";
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+    loader.grub = {
+        enable = true;
+        device = "/dev/vda";
+    };
+  };
   # Slows down write operations considerably
   nix.settings.auto-optimise-store = false;
-
   boot.binfmt.emulatedSystems = [ "aarch64-linux" "i686-linux" ];
+
+  system.stateVersion = "23.05";
 }
 
