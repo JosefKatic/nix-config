ceac292b93c1b9d1447eec84703f736f7b34fcad
Josef Katič <josef@joka00.dev>
2024-10-02 00:57:28 +0200

    Update nixos-config hirundo

--- config/nixos/company.nix
+++ config/nixos/company.nix
@@ -3,6 +3,7 @@
     autoUpgrade = {
       enable = true;
       dates = "hourly";
+      flake = "github:JosefKatic/nix-config"; # Is used for home-manager-setup
       oldFlakeRef = "self";
     };
   };
