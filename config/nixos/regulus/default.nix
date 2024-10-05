786e0463eff3c4281d9634c70af961853abb259a
Josef Katič <josef@joka00.dev>
2024-10-05 14:17:39 +0200

    hosts/falco: Remove init

--- config/nixos/regulus/default.nix
+++ config/nixos/regulus/default.nix
@@ -15,9 +15,6 @@
     };
     home = {
       users = ["joka" "joka-ipa"];
-      init = {
-        url = "https://github.com/JosefKatic/nix-config";
-      };
     };
     core = {
       disableDefaults = true;
