e39a32c5ac8c4ebd0decc35cb75fd5cf00c5ac45
Josef Katič <josef@joka00.dev>
2024-11-17 23:15:23 +0100

    Add missing options to generated.nix

--- config/home/hirundo/joka/generated.nix
+++ config/home/hirundo/joka/generated.nix
@@ -1,16 +1,19 @@
 {
   user.name = "joka";
+  user.desktop.programs.browsers.brave.enable = false;
   user.desktop.programs.browsers.firefox.enable = true;
   user.desktop.programs.browsers.chromium.enable = true;
   user.desktop.programs.editors.vscode.enable = true;
+  user.desktop.programs.emulators.alacritty.enable = false;
   user.desktop.programs.emulators.kitty.enable = true;
+  user.desktop.programs.emulators.wezterm.enable = false;
   user.desktop.programs.games.lutris.enable = true;
   user.desktop.programs.games.minecraft.enable = true;
   user.desktop.programs.media.mpv.enable = true;
+  user.desktop.programs.productivity.zathura.enable = true;
   user.desktop.services.kdeconnect.enable = true;
   user.desktop.services.mako.enable = true;
-  user.desktop.services.polkit-agent.enable = true;
-  user.desktop.services.rnnoise.enable = true;
+  user.desktop.services.polkit_agent.enable = true;
   user.desktop.wayland.hyprland.enable = true;
   user.desktop.wayland.hyprland.services.anyrun.enable = true;
   user.desktop.wayland.hyprland.services.hypridle.enable = true;
