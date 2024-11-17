0a2632811fb4c7e3bf80b907394710e46fae81cf
Josef Katič <josef@joka00.dev>
2024-11-17 13:03:44 +0100

    Fix hirundo generated.nix

--- config/nixos/hirundo/static.nix
+++ config/nixos/hirundo/static.nix
@@ -10,6 +10,19 @@
   device.boot.uefi.enable = true;
   device.boot.uefi.secureboot = true;
   device.home.users = ["joka" "joka-ipa"];
+  device.hardware.bluetooth.enable = true;
+  device.hardware.bluetooth.enableManager = false;
+  device.hardware.cpu.amd.enable = true;
+  device.hardware.cpu.intel.enable = false;
+  device.hardware.disks.hdd.enable = false;
+  device.hardware.disks.ssd.enable = true;
+  device.hardware.gpu.amd.enable = true;
+  device.hardware.gpu.intel.enable = false;
+  device.hardware.gpu.nvidia.enable = false;
+  device.hardware.misc.trezor.enable = true;
+  device.hardware.misc.xbox.enable = true;
+  device.hardware.misc.yubikey.enable = true;
+
   services.nordvpn.enable = true;
   programs.adb.enable = true;
 }
