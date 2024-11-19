09e564a58d6f6159c4060bc6caca5cfdc1064e69
Josef Katič <josef@joka00.dev>
2024-11-20 00:55:54 +0100

    Migrate falco

---
+++ config/nixos/falco/static.nix
@@ -0,0 +1,11 @@
+{
+  nixpkgs.hostPlatform = "aarch64-linux";
+  boot.initrd.availableKernelModules = ["xhci_pci" "virtio_net" "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_scsi" "9p" "9pnet_virtio"];
+  boot.initrd.kernelModules = ["virtio_balloon" "virtio_console" "virtio_rng" "virtio_gpu"];
+  device.type = "server";
+  device.virtualized = true;
+  device.boot.quietboot.enable = true;
+  device.boot.uefi.enable = true;
+  device.boot.uefi.secureboot = false;
+  device.home.users = ["joka" "joka-ipa"];
+}
