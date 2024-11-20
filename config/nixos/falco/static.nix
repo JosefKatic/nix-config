71c4e8c6410ccdd4ae96fee2bcba136909fb7f63
Josef Katič <josef@joka00.dev>
2024-11-20 02:50:56 +0100

    Fix aarch64

--- config/nixos/falco/static.nix
+++ config/nixos/falco/static.nix
@@ -2,11 +2,8 @@
   nixpkgs.hostPlatform = "aarch64-linux";
   boot.initrd.availableKernelModules = ["virtio_net" "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_scsi" "9p" "9pnet_virtio"];
   boot.initrd.kernelModules = ["virtio_balloon" "virtio_console" "virtio_rng" "virtio_gpu"];
-  device.type = "server";
-  device.virtualized = true;
-  device.boot.quietboot.enable = true;
+  device.boot.quietboot.enable = false;
   device.boot.uefi.enable = true;
-  device.boot.uefi.secureboot = false;
   device.home.users = ["joka" "joka-ipa"];
   device.core.kernel = "linux_6_11";
 }
