7bedc334cefa3081a5464cc10c103e421458a39d
Josef Katič <josef@joka00.dev>
2025-04-16 19:46:14 +0200

    Try to add another user again

---
+++ config/home/hirundo/joka-ipa/static.nix
@@ -0,0 +1,24 @@
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  theme = {
+    wallpaper = inputs.joka00-modules.legacyPackages.${pkgs.system}.wallpapers.astronaut-minimalism.outPath;
+    colorscheme.type = "content";
+  };
+  user = {
+    name = "joka-ipa";
+    desktop.monitors = [
+      {
+        name = "eDP-1";
+        width = 1920;
+        height = 1080;
+        primary = true;
+        workspace = "1";
+      }
+    ];
+    desktop.programs.productivity.proton.pass.enable = true;
+    desktop.programs.productivity.proton.mail.enable = true;
+  };
+}
