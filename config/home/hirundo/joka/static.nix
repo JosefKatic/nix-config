e9fbb2020a9ee342642bcfa691c93015bf3d83c7
Josef Katič <josef@joka00.dev>
2025-05-28 16:46:03 +0200

    Update: flake.lock

--- config/home/hirundo/joka/static.nix
+++ config/home/hirundo/joka/static.nix
@@ -5,7 +5,6 @@
 }: {
   theme = {
     wallpaper = inputs.joka00-modules.legacyPackages.${pkgs.system}.wallpapers.binary-black.outPath;
-    colorscheme.type = "monochrome";
   };
   user = {
     name = "joka";
