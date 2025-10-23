5fc509381e003a95cc612e1aa12bdd344dc06c4d
Josef Katič <josef@joka00.dev>
2025-10-23 09:29:56 +0200

    bump: flake.lock

--- flake.nix
+++ flake.nix
@@ -3,10 +3,12 @@
     extra-substituters = [
       "https://hyprland.cachix.org"
       "https://cache.joka00.dev"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cache.joka00.dev:ELw0BiKSycBVWYgv0lFW+Uqjez0Y9gnKEh7sQ/8eHvE="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
   inputs = {
